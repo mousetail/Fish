b8bc993527e6342422b778128973582ced01d4e9
Maurits van Riezen (mousetail) <mousetail@mousetail.nl>
2022-11-07 11:18:48 +0100

    add script

---
+++ src/scripts/update_code_info.ts
@@ -0,0 +1,14 @@
+const code_info = document.getElementById('code-info') as HTMLDivElement;
+
+export function start_code_info_event_listeners(code: HTMLTextAreaElement) {
+    const on_input = () => {
+        let length = code.value.length;
+        let cursor_position = code.selectionStart;
+        let area_before = code.value.substring(0, cursor_position).split("\n");
+
+        code_info.innerText = `Cursor at [${area_before[area_before.length - 1].length},${area_before.length - 1}], ${length} chars.`
+    }
+    code.addEventListener('input', on_input);
+    on_input();
+
+}
